0f18f753951296e39d2b03abe471f2870b59d233
jackie <mgjackline@gmail.com>
2019-01-09 16:52:22 +0300

    [Starts #163065410] adds get a specific question tests

--- app/tests/v1/test_meetups.py
+++ app/tests/v1/test_meetups.py
@@ -43,6 +43,7 @@ class TestMeetups(unittest.TestCase):
         self.assertTrue(result["data"])
         self.assertEqual(response.status_code, 200)
 
+
     #Test to display a meetup record
     def test_get_specific_meetup(self):
         response = self.client.post('api/v1/meetups/1',data=json.dumps(self.meetup2),content_type="application/json")
